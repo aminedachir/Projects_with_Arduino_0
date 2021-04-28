8233570208c5d475199763d66bf855e08c6e7f36
aminedachir <dachiramine110@gmail.com>
2021-04-28 20:48:51 +0100

    Displayright

---
+++ lcd2/lcd2.ino
@@ -0,0 +1,16 @@
+#include "LiquidCrystal.h"
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+void setup(){
+  lcd.begin(2,2);
+  int a = 0;
+  for (int i=0;i<16;i++){
+    lcd.setCursor(a,0);
+    delay(500);
+    lcd.print("hello");
+    a = a + 1;
+    delay(500);
+    lcd.clear();
+  }
+}
+void loop(){
+}
