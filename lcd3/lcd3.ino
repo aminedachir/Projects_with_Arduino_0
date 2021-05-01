310afd7988aa65196d568a3f02a6c1afcd650f6a
aminedachir <dachiramine110@gmail.com>
2021-05-01 17:52:59 +0100

    commentaire

--- lcd3/lcd3.ino
+++ lcd3/lcd3.ino
@@ -7,15 +7,15 @@ void setup(){
   lcd.setCursor(0,0);
   lcd.print("amine");
   int a = 0;
-  int b = -1;
+  int b = 0;
 }
 void loop() {
-  int a=a+1;
-  int b=b+1;
+  int a=a;
+  int b=b;
     int x;
     x = analogRead(0);
-    if (x < 60) {lcd.clear();delay(1000);lcd.setCursor(a,0);lcd.print("amine");}
-    else if(x<200){lcd.clear();delay(1000);lcd.setCursor(a,b);lcd.print("amine");}
-    else if(x<400){b=b+1;lcd.clear();delay(1000);lcd.setCursor(a,b);lcd.print("amine");}
-    else if(x<600){a=a-1;lcd.clear();delay(1000);lcd.setCursor(a,0);lcd.print("amine");}   
+    if (x < 60) {lcd.clear();delay(1000);lcd.setCursor(a+1,0);lcd.print("amine");}
+    // else if(x<200){lcd.clear();delay(1000);lcd.setCursor(a,b);lcd.print("amine");}
+    // else if(x<400){b=b+1;lcd.clear();delay(1000);lcd.setCursor(a,b);lcd.print("amine");}
+    // else if(x<600){a=a-1;lcd.clear();delay(1000);lcd.setCursor(a,0);lcd.print("amine");}  
 } 
