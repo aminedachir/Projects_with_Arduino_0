5882a48c69efa577e99621a3bdab5bd0dc1d6640
aminedachir <dachiramine110@gmail.com>
2021-05-01 02:50:06 +0100

    MODIFICATIONS

--- lcd3/lcd3.ino
+++ lcd3/lcd3.ino
@@ -4,14 +4,18 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 void setup(){
   lcd.begin(2,2);
+  lcd.setCursor(0,0);
+  lcd.print("amine");
+  int a = 0;
+  int b = -1;
 }
 void loop() {
- int x;
- x = analogRead (0);
- if (x < 60) {lcd.setCursor(3,1);lcd.print("amine");}
- else if (x < 200) {lcd.setCursor(0,0);lcd.print("amine");}
- else if (x < 400){lcd.setCursor(0,1);lcd.print("amine");}
- else if (x < 600){lcd.setCursor(0,1);lcd.print("amine");}
- delay(1500);
- lcd.clear();
+  int a=a+1;
+  int b=b+1;
+    int x;
+    x = analogRead(0);
+    if (x < 60) {lcd.clear();delay(1000);lcd.setCursor(a,0);lcd.print("amine");}
+    else if(x<200){lcd.clear();delay(1000);lcd.setCursor(a,b);lcd.print("amine");}
+    else if(x<400){b=b+1;lcd.clear();delay(1000);lcd.setCursor(a,b);lcd.print("amine");}
+    else if(x<600){a=a-1;lcd.clear();delay(1000);lcd.setCursor(a,0);lcd.print("amine");}   
 } 
