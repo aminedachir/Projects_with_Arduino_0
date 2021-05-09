cd6e0622e98743bd1502b4713328193bcee038bd
aminedachir <dachiramine110@gmail.com>
2021-05-09 23:18:31 +0100

    Update Time

--- lcd4/lcd4.ino
+++ lcd4/lcd4.ino
@@ -2,14 +2,19 @@
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 void setup(){
   lcd.begin(2,2);
-  int i=0;
-}
-void loop(){
-  for(int i=i;i<5;i++){
-    delay(500);
-    lcd.setCursor(0,0);
+  for (int i=0;i<15;i++){
+    delay(400);
+    lcd.setCursor(3,0);
     lcd.print(i);
     delay(500);
-    lcd.clear();
   }
+}
+void loop(){
+  //delay(1500);
+  //for(int i=i;i<5;i++){
+    //lcd.setCursor(0,0);
+    //lcd.print("1");
+    //delay(1500);
+    //lcd.clear();
+  //}
 }
