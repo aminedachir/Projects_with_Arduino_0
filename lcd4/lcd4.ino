c3c5b3b7e980c39a3118e52b4e1dc4b67fc83cc3
aminedachir <dachiramine110@gmail.com>
2021-05-11 21:15:37 +0100

    Update

--- lcd4/lcd4.ino
+++ lcd4/lcd4.ino
@@ -1,5 +1,13 @@
 #include "LiquidCrystal.h"
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+void loop(){
+  int x;
+  x = analogRead(0);
+  if(x<600){lcd.setCursor(0,0);lcd.print("amine");}
+  delay(2000);
+  lcd.clear();
+}
+
 void setup(){
   lcd.begin(2,2);
   lcd.setCursor(9,0);
@@ -19,6 +27,4 @@ void setup(){
     if(i==20){i=0;a=a+1;lcd.setCursor(7,0);lcd.print(a);}
     else if(a==12){i=0;a=0;lcd.setCursor(4,0);lcd.print(b);}
   }
-}
-void loop(){
 }
