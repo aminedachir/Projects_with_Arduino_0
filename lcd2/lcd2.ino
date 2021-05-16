a90e90fc92e014905599750c53d60e6c3d3f6e04
aminedachir <dachiramine110@gmail.com>
2021-05-16 22:56:16 +0100

    Update loop

--- lcd2/lcd2.ino
+++ lcd2/lcd2.ino
@@ -1,13 +1,13 @@
 #include "LiquidCrystal.h"
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 void setup(){
+  lcd.begin(2,2);
 }
 void loop(){
-  lcd.begin(2,2);
   int a = 0;int b = -2;
-  for (int i=0;i<16;i++){
+  for (int i=0;i<17;i++){
     lcd.setCursor(a,0);
-    delay(500);
+    delay(400);
     lcd.print("hello");
     a = a + 1;
     b = b + 1;
