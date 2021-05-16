2147bf674e112004dcbd8c270feb465615f3c0c3
aminedachir <dachiramine110@gmail.com>
2021-05-16 22:23:35 +0100

    Delete one character

--- lcd2/lcd2.ino
+++ lcd2/lcd2.ino
@@ -2,14 +2,18 @@
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 void setup(){
   lcd.begin(2,2);
-  int a = 0;
+  int a = 0;int b = 0;
   for (int i=0;i<16;i++){
     lcd.setCursor(a,0);
     delay(200);
     lcd.print("hello");
     a = a + 1;
     delay(500);
-    lcd.clear();
+    b = a - 1;
+    for (int i=0;i<a){
+        lcd.setCursor(b,0);
+      }
+    
   }
 }
 void loop(){
